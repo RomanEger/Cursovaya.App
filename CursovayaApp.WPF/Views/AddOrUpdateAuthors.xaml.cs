f747605ad89b4a65a2e4f63731422484a149eaed
RomanEger <egerromaan@gmail.com>
2024-02-06 21:04:47 +0600

    /add new view page

--- CursovayaApp.WPF/Views/AddOrUpdateAuthors.xaml.cs
+++ CursovayaApp.WPF/Views/AddOrUpdateAuthors.xaml.cs
@@ -1,10 +1,11 @@
 ﻿using System.Windows;
+using CursovayaApp.WPF.Models.DbModels;
 
 namespace CursovayaApp.WPF.Views;
 
 public partial class AddOrUpdateAuthors : Window
 {
-    public AddOrUpdateAuthors()
+    public AddOrUpdateAuthors(Author author)
     {
         InitializeComponent();
     }
